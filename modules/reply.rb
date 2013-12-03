745312934f31d9dd2c1342a368374a543cc9cf8b
hydride0 <hydride0@gmail.com>
2013-12-03 20:45:57 +0100

    Add replies

---
+++ modules/reply.rb
@@ -0,0 +1,26 @@
+##
+##            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+##                    Version 2, December 2004
+## 
+## Everyone is permitted to copy and distribute verbatim or modified
+## copies of this license document, and changing it is allowed as long
+## as the name is changed.
+## 
+##            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+##   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+## 
+##  0. You just DO WHAT THE FUCK YOU WANT TO.
+##
+
+class Descartes
+	class Reply
+		include Cinch::Plugin
+
+		match /Descartes.*\?/, :use_prefix => false
+
+		def execute(m)
+			replies = [].tap {|ary| File.read('modules/reply/replies.txt').each_line { |line| ary << line } }
+			m.reply replies[rand(1..20)]
+		end
+	end
+end	
