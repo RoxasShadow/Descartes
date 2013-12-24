ec8971f8205033d7beb91d5b4e43c0e7225a5bc2
hydride0 <hydride0@gmail.com>
2013-12-24 10:22:03 +0100

    Modify url.rb and add dicteng.rb (even if it's shitty)

---
+++ lib/descartes/modules/dicteng.rb
@@ -0,0 +1,30 @@
+# encoding: UTF-8
+##
+###            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+###                    Version 2, December 2004
+### 
+### Everyone is permitted to copy and distribute verbatim or modified
+### copies of this license document, and changing it is allowed as long
+### as the name is changed.
+### 
+###            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+###   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+### 
+###  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+require 'nokogiri'
+require 'open-uri'
+
+class Descartes
+	class Dicteng
+		include Cinch::Plugin
+
+		match /([a-zA-Z]+) ([a-zA-Z]+)/, :use_prefix => false
+
+		def execute(m, action, word)
+				url = "http://www.wordreference.com/#{action}/#{word}"
+				page = Nokogiri::HTML(open(url))
+        m.reply "#{page.xpath('//table[@class="WRD"]/tr[2]').text}  di più qui: #{url}"
+		end
+	end
+end
