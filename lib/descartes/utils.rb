abefcea0195293a5501f158d81c5182ec85f4493
Giovanni Capuano <webmaster@giovannicapuano.net>
2014-02-06 16:56:32 +0100

    utils: update date translation

--- lib/descartes/utils.rb
+++ lib/descartes/utils.rb
@@ -15,8 +15,8 @@
 
 class String
   def to_ita
-    day_eng = [ 'Sundays',   'Mondays', 'Tuesdays', 'Wednesdays', 'Thursdays', 'Fridays',  'Saturdays' ]
-    day_ita = [ 'Domenica',  'Lunedì',  'Martedì',  'Mercoledì',  'Giovedì',   'Venerdì',  'Sabato'    ]
+    day_eng = [ 'Sunday',   'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',  'Saturday' ]
+    day_ita = [ 'Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì',  'Venerdì', 'Sabato'   ]
     tim_eng = [ 'days',   'hours', 'minutes', 'seconds' ]
     tim_ita = [ 'giorni', 'ore',   'minuti',  'secondi' ]
 
--- lib/descartes/version.rb
+++ lib/descartes/version.rb
@@ -14,6 +14,6 @@
 
 class Descartes
   def self.version
-    '0.7.2'
+    '0.7.3'
   end
 end
