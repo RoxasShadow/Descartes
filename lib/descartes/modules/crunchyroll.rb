fc837b2082c385050640958bc264a72c571ba9cd
Giovanni Capuano <webmaster@giovannicapuano.net>
2014-02-20 22:08:57 +0100

    crunchyroll: get today's releases if args=0

--- lib/descartes/modules/crunchyroll.rb
+++ lib/descartes/modules/crunchyroll.rb
@@ -18,16 +18,29 @@ require 'crunchyroll'
 class Descartes
   class Crunchybot
     include Cinch::Plugin
-    match /\.cr (.+)/, use_prefix: false
 
-    def execute(m)
-      series = Crunchyroll::find m.params[1].split('.cr ')[1]
+    match  /\.cr$/, use_prefix: false, method: :today
+    def today(m)
+      crunchyroll   = Crunchyroll.today
+      not_aired     = crunchyroll.reject            { |r| r[:left].include? 'days' }
+      already_aired = (crunchyroll - not_aired).map { |r| r[:title].colorize       }.flatten.join(', ')[0..-2]
+
+      m.reply "Gli anime di oggi su #{'Crunchyroll'.colorize}:"
+
+      not_aired.each { |series|
+        m.reply "#{series[:title].colorize} (tra #{series[:left].to_ita.colorize})"
+      }
+      
+      m.reply "Sono stati già trasmessi: #{already_aired}."
+    end
+
+    match  /\.cr (.+)/, use_prefix: false, method: :get
+    def get(m)
+      series = Crunchyroll.get m.params[1].split('.cr ')[1]
       
       if series
-        # m.reply "#{series[:title].colorize} is a series airing on #{series[:where].colorize} at #{series[:day].colorize} #{series[:hour].colorize}:#{series[:min].colorize} which is in #{series[:left].colorize}."
         m.reply "#{series[:title].colorize} è una serie trasmessa da #{series[:where].colorize} il #{series[:day].to_ita.colorize} alle #{series[:hour].colorize}:#{series[:min].colorize}, cioè tra #{series[:left].to_ita.colorize}."
       else
-        # m.reply 'Anime not found'.colorize
         m.reply 'Anime non trovato'.colorize
       end
     end
