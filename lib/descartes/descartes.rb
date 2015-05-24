8bfdee7fe1809359ba3c844d71551f4decda320a
RoxasShadow <webmaster@giovannicapuano.net>
2015-05-24 21:55:31 +0200

    descartes: add logging in module loading

--- lib/descartes/descartes.rb
+++ lib/descartes/descartes.rb
@@ -1,28 +1,39 @@
 ##
 #            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 #                    Version 2, December 2004
-# 
+#
 # Everyone is permitted to copy and distribute verbatim or modified
 # copies of this license document, and changing it is allowed as long
 # as the name is changed.
-# 
+#
 #            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
 #   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-# 
+#
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
 ##
 
 class Descartes
-  def self.load(options)
-    $options = options
+  class << self
+    def load(options = {})
+      $options = options
+      $options[:exclude] ||= []
 
-    Dir.glob(File.expand_path('../modules/*.rb', __FILE__)).each { |plugin|
-      name = plugin.split(?/).last.split(?.).first.downcase
-      require plugin unless $options[:exclude].include? name
-    }
+      puts 'Importing modules..'
+      Dir.glob(File.expand_path('../modules/*.rb', __FILE__)).each do |module|
+        name = module.split(?/).last.split(?.).first.downcase
 
-    return Descartes.constants.map { |p|
-      "Descartes::#{p}".split('::').inject(Object) { |o, c| o.const_get c }
-    }
+        if $options[:exclude].include?(name)
+          puts "- Skipping #{name}..."
+        else
+          print "- Loading #{name}..."
+          require module
+          puts ' done'
+        end
+      end
+
+      return Descartes.constants.map do |p|
+        "Descartes::#{p}".split('::').inject(Object) { |o, c| o.const_get c }
+      end
+    end
   end
 end
